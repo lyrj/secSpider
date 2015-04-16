02daeb107834a2ef01fc4eff1aa852fbea2a68b2
wswuai <xiaogmy@gmail.com>
2015-04-16 16:46:00 +0800

    spider can use pipeline component.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,21 +12,11 @@ import scala.util.control.NonFatal
   object Main {
   def main(args: Array[String]): Unit = {
 	  ReadingConfig.argList = args toList
-	  val str = args(0).toString
 	  val system = ActorSystem("Main")
 	  try {
-		  val app = system.actorOf(Props(classOf[com.kingdee.safe.core.Scheduler]),"app")
-		  val terminator = system.actorOf(Props(classOf[Terminator], app), "app-terminator")
+		  system.actorOf(Props(classOf[com.kingdee.safe.core.Scheduler]),"app")
 	  } catch {
 		  case NonFatal(e) => system.shutdown(); throw e
 	  }
   }
-	class Terminator(app: ActorRef) extends Actor with ActorLogging {
-		context watch app
-		def receive = {
-			case Terminated(_) =>
-				log.info("application supervisor has terminated, shutting down")
-				context.system.shutdown()
-		}
-	}
 }
